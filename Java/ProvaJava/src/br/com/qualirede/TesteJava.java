4d2327d2df9f61bef62230b64c61b83d2b3ccdb7
Marcos Michel <mmluciano@hotmail.com>
2019-10-13 22:14:38 -0300

    Projeto Java - Autorizador

---
+++ Java/ProvaJava/src/br/com/qualirede/TesteJava.java
@@ -0,0 +1,38 @@
+package br.com.qualirede;
+
+import java.util.Scanner;
+
+
+public class TesteJava {
+   //pacotes
+	// classes
+	 // metodos
+	  //variaveis e constantes
+	
+	public static void main (String[] args) {
+		/*
+		 * Scanner ler = new Scanner (System.in);
+		 * int    idade;
+		String procedimento = "java Autorizador 1234 10 M",
+		       permitido;
+		char   sexo;
+		
+		
+		
+		System.out.println("Digite a entrada");
+		Scanner ler = new Scanner (System.in);
+		idade = in.nextInt();
+		
+		System.out.println (procedimento);
+		
+		if (procedimento == in ) {
+			System.out.println("SIM");
+		}else {
+			System.out.println("NÃO");
+		}*/
+		
+		
+			
+	}
+	
+}
